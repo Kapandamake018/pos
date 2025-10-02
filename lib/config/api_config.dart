2c1f57aa9b8e5a79dfb94aa2d504c65c74466bbe
Tumbikani Thomas Kapandamake <kapandathomas0@gmail.com>
2025-10-02 22:24:56 +0200

    changed netwok

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,6 +1,6 @@
 class ApiConfig {
-  //static const String baseUrl = 'http://10.0.2.2:8000';  // Android emulator
-  static const String baseUrl = 'http://localhost:8000'; // iOS simulator
+  static const String baseUrl = 'http://10.0.2.2:8000'; // Android emulator
+  //static const String baseUrl = 'http://localhost:8000'; // iOS simulator
 
   // API endpoints
   static const String login = '/login';
